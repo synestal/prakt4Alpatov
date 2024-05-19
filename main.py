99c93fc8505791b12725f2012d3f96754e93019a
synestal <103376087+synestal@users.noreply.github.com>
2024-05-19 18:57:34 +0300

    Реализован базовый функционал - считывание из переменной

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+
+def normalize_phone_numbers(text):
+    # Паттерн для поиска телефонных номеров
+    phone_pattern = re.compile(r'\+?\d{1,3}\s?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{2}[-\s]?\d{2}')
+
+    def repl(match):
+        # Извлекаем номер телефона из найденного совпадения
+        phone_number = match.group(0)
+
+        # Извлекаем код страны из номера (первая цифра)
+        country_code = '+' + phone_number[1]
+
+        # Удаляем все символы, кроме цифр из номера
+        digits = re.sub(r'\D', '', phone_number)
+
+        # Форматируем номер телефона
+        formatted_number = '{} ({}) {}-{}-{}'.format(country_code, digits[1:4], digits[4:7], digits[7:9], digits[9:])
+        return formatted_number
+
+    # Замена всех найденных номеров в тексте
+    normalized_text = re.sub(phone_pattern, repl, text)
+    return normalized_text
+
+# Пример использования
+input_text = """
+Раз два три:
++14567890123, аываы в+4567890123456, ываыв а+78901234567ываыв аыва ыва ыва.
+"""
+
+normalized_text = normalize_phone_numbers(input_text)
+print("Нормализованный текст:")
+print(normalized_text)
