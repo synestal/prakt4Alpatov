159843e6f381906fe1e08c15d6339762f0a6c747
synestal <103376087+synestal@users.noreply.github.com>
2024-05-19 19:45:21 +0300

    Переведен на 1 этап разработки

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+"""Телефонные номера"""
 import re
 
 
 def normalize_phone_numbers(text):
+    """Нормализатор"""
     # Паттерн для поиска телефонных номеров
     phone_pattern = re.compile(r'\+?\d{1,3}\s?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{2}[-\s]?\d{2}')
 
     def repl(match):
+        """Proceed"""
         # Извлекаем номер телефона из найденного совпадения
         phone_number = match.group(0)
 
@@ -16,20 +19,21 @@ def normalize_phone_numbers(text):
         digits = re.sub(r'\D', '', phone_number)
 
         # Форматируем номер телефона
-        formatted_number = '{} ({}) {}-{}-{}'.format(country_code, digits[1:4], digits[4:7], digits[7:9], digits[9:])
+        formatted_number = ('{} ({}) {}-{}-{}'
+                            .format(country_code, digits[1:4], digits[4:7], digits[7:9], digits[9:]))
         return formatted_number
 
     # Замена всех найденных номеров в тексте
-    normalized_textHEre = re.sub(phone_pattern, repl, text)
-    return normalized_textHEre
+    normalized_texthere = re.sub(phone_pattern, repl, text)
+    return normalized_texthere
 
 
 # Пример использования
-input_text = """
+INPUT_TEXT = """
 Раз два три:
 +14567890123, аываы в+4567890123456, ываыв а+78901234567ываыв аыва ыва ыва.
 """
 
-normalized_text = normalize_phone_numbers(input_text)
+normalized_text = normalize_phone_numbers(INPUT_TEXT)
 print("Нормализованный текст:")
 print(normalized_text)
